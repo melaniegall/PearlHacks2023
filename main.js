0dc60ab0cbb60d374fa49b1caacd858507f4a865
melaniegall <115383687+melaniegall@users.noreply.github.com>
2023-02-11 14:23:34 -0500

    mel commit

--- main.js
+++
@@ -1,9 +0,0 @@
-var frog = document.getElementById("frog")
-var frog_emoji = "🐸"
-frog.onclick = function(){
-	if(!frog.innerhtml.includes(frog_emoji)){
-		frog.innerhtml = frog.innerhtml + frog_emoji
-	} else {
-		frog.innerhtml = 'frog'
-	}
-}
