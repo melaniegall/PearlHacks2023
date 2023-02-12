3925eda5615dad3eab6b76969a6cbcc9569c4f8d
melaniegall <115383687+melaniegall@users.noreply.github.com>
2023-02-11 22:57:36 -0500

    comparedates

--- main.js
+++ main.js
@@ -5,5 +5,16 @@ yes.onclick = function() {
 		yes.innerHTML = "yes"
 	} else {
 		yes.innerHTML = yes.innerHTML + yes_emoji
-	}
-}
+	}}
+//document.appendChild(element)
+
+
+var userbday = document.getElementById("userbday");
+var partnerbday = document.getElementById("partnerbday");
+var submitbutton = document.getElementById("submitbutton")
+submitbutton.onclick = function() {
+	if(userbday.value == partnerbday.value) {
+		document.getElementById("p1").innerHTML = "The same!";
+	} else {
+		document.getElementById("p1").innerHTML = "Not the same!";
+	}}
