be498dc9070b897d3c1a436c29c7cce332a2e982
melaniegall <115383687+melaniegall@users.noreply.github.com>
2023-02-11 23:59:23 -0500

    Fully functional but ugly font rip

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var yes = document.getElementById("yes");
-var yes_emoji = "🐸"
-yes.onclick = function() {
-	if(yes.innerHTML.includes(yes_emoji)) {
-		yes.innerHTML = "yes"
-	} else {
-		yes.innerHTML = yes.innerHTML + yes_emoji
-	}}
+//var yes = document.getElementById("yes");
+//var yes_emoji = "🐸"
+//yes.onclick = function() {
+//	if(yes.innerHTML.includes(yes_emoji)) {
+//		yes.innerHTML = "yes"
+//	} else {
+//		yes.innerHTML = yes.innerHTML + yes_emoji
+//	}}
 //document.appendChild(element)
 function playAudio(url) {
    new Audio(url).play();
@@ -17,7 +17,7 @@ var partnerbday = document.getElementById("partnerbday");
 var submitbutton = document.getElementById("submitbutton")
 submitbutton.onclick = function() {
 	if(userbday.value == partnerbday.value) {
-		document.getElementById("p1").innerHTML = "The same!";
+		document.getElementById("p1").innerHTML = "Twin slayyy";
 	} else {
-		document.getElementById("p1").innerHTML = "Not the same!";
+		document.getElementById("p1").innerHTML = "Not twins? Ugly.";
 	}}
